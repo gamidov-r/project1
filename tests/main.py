014ba17250a4e6d6ad26d885f046c8db95467d94
ruslan <gamidov.r@mail.ru>
2025-06-17 14:39:22 +0300

    test

--- tests/main.py
+++ tests/main.py
@@ -1,4 +1,4 @@
-import masks
+import masks, widget
 
 def check_card_mask(card: int, result: str) -> bool:
     """Проверка функции маскировки карты"""
@@ -21,3 +21,6 @@ def check_account_mask(account: int, result: str) -> bool:
 if __name__ == "__main__":
     print(check_card_mask(7000792289606361, "7000 79** **** 6361"))
     print(check_account_mask(73654108430135874305, "**4305"))
+
+print(widget.mask_account_card('Visa Platinum 7000792289606361'))
+print(widget.mask_account_card('Счет 64686473678894779589'))
