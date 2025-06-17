9ca6c93d723900598eaedebdc338427f1c78cc55
ruslan <gamidov.r@mail.ru>
2025-06-17 19:47:12 +0300

    check codeeby test, ready for push

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,13 @@
-def filter_by_state(dictionaries: list, state: str = 'EXECUTED') -> list:
-    """ сортировка словарей по ключу """
+def filter_by_state(dictionaries: list, state: str = "EXECUTED") -> list:
+    """сортировка словарей по ключу"""
     result = []
     for i in dictionaries:
-        if i.get('state') == state:
+        if i.get("state") == state:
             result.append(i)
     return result
 
 
 def sort_by_date(dictionaries: list, reverse: bool = True) -> list:
-    """ сортировка словарей по дате """
-    dictionaries.sort(reverse=True, key=lambda x: x.get('date'))
+    """сортировка словарей по дате"""
+    dictionaries.sort(reverse=True, key=lambda x: x.get("date"))
     return dictionaries
-
