7e7f4b7b77de58e2d3e9c5ac7e74038d87cc1aa8
ruslan <gamidov.r@mail.ru>
2025-06-17 19:39:07 +0300

    add sort func

--- src/processing.py
+++ src/processing.py
@@ -5,6 +5,20 @@ def filter_by_state(dictionaries: list, state='EXECUTED') -> list:
             result.append(i)
     return result
 
+
+def sort_by_date(dictionaries: list, reverse=True) -> list:
+    dictionaries.sort(reverse=True, key=lambda x: x.get('date'))
+    return dictionaries
+
+    # Выход функции (сортировка по убыванию, т. е. сначала самые последние операции)
+    [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+test = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+print(sort_by_date(test))
+
+
 # Выход функции со статусом по умолчанию 'EXECUTED'
 a=[{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
 
