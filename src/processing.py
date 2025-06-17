2c33c053efcb30c84178494f3047a0611aab994a
ruslan <gamidov.r@mail.ru>
2025-06-17 19:42:11 +0300

    code cleanup, add comments

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
-def filter_by_state(dictionaries: list, state='EXECUTED': str) -> list:
+def filter_by_state(dictionaries: list, state: str = 'EXECUTED') -> list:
+    """ сортировка словарей по ключу """
     result = []
     for i in dictionaries:
         if i.get('state') == state:
@@ -6,22 +7,8 @@ def filter_by_state(dictionaries: list, state='EXECUTED': str) -> list:
     return result
 
 
-def sort_by_date(dictionaries: list, reverse=True) -> list:
+def sort_by_date(dictionaries: list, reverse: bool = True) -> list:
+    """ сортировка словарей по дате """
     dictionaries.sort(reverse=True, key=lambda x: x.get('date'))
     return dictionaries
 
-    # Выход функции (сортировка по убыванию, т. е. сначала самые последние операции)
-    [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-test = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-print(sort_by_date(test))
-
-
-# Выход функции со статусом по умолчанию 'EXECUTED'
-a=[{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-
-# Выход функции, если вторым аргументов передано 'CANCELED'
-b=[{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-print(filter_by_state(a))
