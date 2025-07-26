d42e1cb3f6fb40902c9c0e2831dcf520491a7612
ruslan <gamidov.r@mail.ru>
2025-07-26 13:57:00 +0300

    add utils and external_api

---
+++ src/external_api.py
@@ -0,0 +1,33 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+URL = "https://api.apilayer.com/exchangerates_data/convert"
+
+DEBUG = True  # возврат заглушки, минуя работу с api
+# DEBUG = False  # работа напрямую с api
+
+
+mock = {
+    "success": True,
+    "query": {"from": "EUR", "to": "RUB", "amount": 5},
+    "info": {"timestamp": 1753524555, "rate": 93.035614},
+    "date": "2025-07-26",
+    "result": 465.17807,
+}
+
+
+def convert_currency(current: str, to: str, value: float) -> dict:
+    """Конвертация валюты через API, ответ в виде json запроса"""
+    if DEBUG:
+        return mock
+    params = {"from": current, "to": to, "amount": value, "apikey": API_KEY}
+    response = requests.get(URL, params)
+    json_response = response.json()
+    if json_response["success"]:
+        return json_response
+    else:
+        return {}
