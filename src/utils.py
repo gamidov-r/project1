52ca038d899da769e2dd43e76219eb9c4cdaf869
ruslan <gamidov.r@mail.ru>
2025-07-27 23:11:20 +0300

    add encoding for load json from file, fix fetch result from api

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ OPERATIONS_PATH = "../data/operations.json"
 def load_operations(json_path: str) -> dict[str, Union]:
     """Чтение json файла"""
     try:
-        with open(json_path, "r") as f:
+        with open(json_path, "r", encoding='UTF-8') as f:
             return json.load(f)
     except json.JSONDecoder:
         return {}
@@ -24,7 +24,7 @@ def get_amount(transaction: dict) -> float:
         return float(data["amount"])
     else:
         from_api = external_api.convert_currency(data["currency"]["code"], "RUB", data["amount"])
-        return float(from_api["query"]["amount"])
+        return float(from_api["result"])
 
 
 # transactions = load_operations(OPERATIONS_PATH)
