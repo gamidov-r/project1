465452fa995640071227d38925d9f1ba70256190
ruslan <gamidov.r@mail.ru>
2025-09-10 21:44:12 +0300

    fixed main.py: add questions for user, answer with empty results, etc

--- src/main.py
+++ src/main.py
@@ -65,17 +65,17 @@ def process_bank_search(data: list[dict], search: str) -> tuple[list[dict], list
 
 
 def main() -> None:
-    """Функция по получению параметров сортировки от пользователя"""
+    """Основная функция по получению параметров сортировки от пользователя"""
     welcome = """Программа: Привет! Добро пожаловать в программу работы с банковскими транзакциями. 
     Выберите необходимый пункт меню:
     1. Получить информацию о транзакциях из JSON-файла
     2. Получить информацию о транзакциях из CSV-файла
     3. Получить информацию о транзакциях из XLSX-файла"""
-    global sort_by_date, ascending, only_rub, choiced_filter
+    # global sort_by_date, ascending, only_rub, choiced_filter
     file_types = {"1": "json", "2": "csv", "3": "xlsx"}
     print(welcome)
-    # x = input("Пользователь: ")
-    x = "3"
+    x = input("Пользователь: ")
+    # x = "3"
     if x in file_types:
         print(f"Программа: Для обработки выбран {file_types[x].upper()}-файл.")
     else:
@@ -86,8 +86,8 @@ def main() -> None:
     filter_types = ["EXECUTED", "CANCELED", "PENDING"]
     while 1:
         print(filter_message)
-        # x = input("Пользователь: ").upper()
-        x = "EXECUTED"
+        x = input("Пользователь: ").upper()
+        # x = "EXECUTED"
         if x in filter_types:
             print(f"Программа: Операции отфильтрованы по статусу {x}")
             choiced_filter = x
@@ -109,40 +109,43 @@ def main() -> None:
     pattern_descending = r"(убыв|e,sd|вуысутв|descend)"
 
     print(f"Программа: {questions[0]}")
-    # x = input("Пользователь: ")
-    x = "нет"
+    x = input("Пользователь: ")
+    # x = "нет"
     if pattern_yes in re.findall(pattern_yes, x, re.IGNORECASE):
         sort_by_date = 1
     elif pattern_no in re.findall(pattern_no, x, re.IGNORECASE):
         sort_by_date = 0
 
     print(f"Программа: {questions[1]}")
-    # x = input("Пользователь: ")
-    x = "возраст"
+    x = input("Пользователь: ")
+    # x = "возраст"
     if pattern_ascending in re.findall(pattern_ascending, x, re.IGNORECASE):
         ascending = 1
     elif pattern_descending in re.findall(pattern_descending, x, re.IGNORECASE):
         ascending = 0
 
     print(f"Программа: {questions[2]}")
-    # x = input("Пользователь: ")
-    x = "нет"
+    x = input("Пользователь: ")
+    # x = "нет"
     if pattern_yes in re.findall(pattern_yes, x, re.IGNORECASE):
         only_rub = 1
     elif pattern_no in re.findall(pattern_no, x, re.IGNORECASE):
         only_rub = 0
 
     print(f"Программа: {questions[3]}")
-    # x = input("Пользователь: ")
-    x = "нет"
+    x = input("Пользователь: ")
+    # x = "нет"
     print(f"Программа: {questions[4]}")
     result, trans_ids = process_bank_search(read_transactions_csv("../data/transactions.csv"), "Перевод")
     print(result)
     print(f"Программа\nВсего банковских операций в выборке: {collections.Counter(trans_ids).total()}")
-    for item in result:
-        print(
-            f"{widget.get_date(item["date"])}\n{masks.get_mask_account(item["from"])}\nСумма: {item["amount"]} {item["currency_name"]}\n"
-        )
+    if result:
+        for item in result:
+            print(
+                f"{widget.get_date(item["date"])}\n{masks.get_mask_account(item["from"])}\nСумма: {item["amount"]} {item["currency_name"]}\n"
+            )
+    else:
+        print("Транзакций не найдено")
 
 
 def read_transactions_csv(path_to_csv: str) -> list:
